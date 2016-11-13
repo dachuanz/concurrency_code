6413ebda5914059399eda187047a859be453deb7
张大川 <dachuanz@gmail.com>
2016-11-13 20:45:26 +0800

    Create Test.java

---
+++ Test1/src/extthread/Test.java
@@ -0,0 +1,12 @@
+package extthread;
+
+public class Test {
+	public static void main(String[] args) throws InterruptedException {
+		Object lock = new Object();
+		MyThread1 myThread1 = new MyThread1(lock);
+		myThread1.start();
+		Thread.sleep(3000);
+		MyThread2 myThread2 = new MyThread2(lock);
+		myThread2.start();
+	}
+}
