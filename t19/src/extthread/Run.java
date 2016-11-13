37becd514b40264434c2985d333723d1ff2d4cd9
张大川 <dachuanz@gmail.com>
2016-11-13 15:03:51 +0800

    Update Run.java

--- t19/src/extthread/Run.java
+++ t19/src/extthread/Run.java
@@ -1,15 +1,15 @@
 package extthread;
 
 public class Run {
-public static void main(String[] args) {
-	for (int i = 0; i < 5; i++) {
-		MyThread1 myThread1 = new MyThread1();
-		myThread1.setPriority(1);
-		myThread1.start();
-		MyThread2 myThread2= new MyThread2();
-		myThread2.setPriority(10);
-		myThread2.start();
-		
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			MyThread1 myThread1 = new MyThread1();
+			myThread1.setPriority(1);
+			myThread1.start();
+			MyThread2 myThread2 = new MyThread2();
+			myThread2.setPriority(10);
+			myThread2.start();
+
+		}
 	}
-}
 }
